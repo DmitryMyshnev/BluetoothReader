d7e51f069706b827d5eeb999855b7c021aa736f8
Дмитрий Мишнев <dmitrymishnev@gmail.com>
2020-05-31 10:01:33 +0300

    Add project files.

---
+++ Bluetooth/Bluetooth.Android/BLE.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Bluetooth;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Bluetooth.Droid.BLE))]
+namespace Bluetooth.Droid
+{
+     class BLE :IDeviceControl
+    {
+        public void  BluetoothManager()
+        {
+            BluetoothAdapter adapter = BluetoothAdapter.DefaultAdapter;
+            if (adapter == null)
+                throw new Exception("No Bluetooth adapter found.");
+
+            if (!adapter.IsEnabled)
+                // throw new Exception("Bluetooth adapter is not enabled.");
+                adapter.Enable();
+        }
+    }
+}
---
+++ Bluetooth/Bluetooth/IDeviceControl.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bluetooth
+{
+     public interface IDeviceControl
+    {
+        void BluetoothManager();
+    }
+}
